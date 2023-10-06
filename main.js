fa21b1e84cdc688c0cdba5fd89b669c11083f9ce
Kris <kharonek@yahoo.com>
2023-10-06 19:27:01 -0300

    completed basic game implementation

--- main.js
+++ main.js
@@ -2,6 +2,10 @@ function getRandomItem(array) {
   return array[Math.floor(Math.random() * array.length)];
 }
 
+function capitalizeFirstLetter(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
 function getComputerChoice() {
   const actions = ["rock", "paper", "scissors"];
 
@@ -9,9 +13,69 @@ function getComputerChoice() {
 }
 
 function playRound(playerSelection, computerSelection) {
-    // Sanitise player input
-    playerSelection = playerSelection.toLowerCase();
-    console.log(playerSelection);
+  // Sanitise player input
+  playerSelection = playerSelection.toLowerCase();
+
+  let playerWon = false;
+
+  if (playerSelection === computerSelection) {
+    return "d";
+  } else if (
+    (playerSelection === "rock" && computerSelection === "scissors") ||
+    (playerSelection === "paper" && computerSelection === "rock") ||
+    (playerSelection === "scissors" && computerSelection === "paper")
+  ) {
+    playerWon = true;
+  }
+
+  if (playerWon) {
+    return "p";
+  } else {
+    return "c";
+  }
 }
 
-playRound("ROCK", getComputerChoice());
+function play() {
+  let playerScore = 0;
+  let computerScore = 0;
+
+  for (let index = 0; index < 5; index++) {
+    console.log("Round", index + 1);
+    let playerSelection = prompt("Enter your choice: ");
+    let computerSelection = getComputerChoice();
+
+    console.log("Player choice: ", playerSelection);
+    console.log("Computer choice: ", computerSelection);
+
+    const results = playRound(playerSelection, computerSelection);
+
+    switch (results) {
+      case "d":
+        console.log("Draw...");
+        break;
+      case "p":
+        console.log(
+          `You win! ${capitalizeFirstLetter(
+            playerSelection
+          )} beats ${capitalizeFirstLetter(computerSelection)}!`
+        );
+        playerScore++;
+        break;
+      case "c":
+        console.log(
+          `You loose! ${capitalizeFirstLetter(
+            computerSelection
+          )} beats ${capitalizeFirstLetter(playerSelection)}!`
+        );
+        computerScore++;
+        break;
+    }
+    console.log(" ");
+
+  }
+
+  console.log("Player Score: ", playerScore);
+  console.log("Computer Score: ", computerScore);
+}
+
+play();
