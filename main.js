c7fb3e787e393db96008c2984c5b101199026def
Kris <kharonek@yahoo.com>
2023-10-07 23:13:52 -0300

    remove game logic that only plays 5 rounds of the game
    Rounds cannot be incremented using a for loop like until this point, due to UI components being added.

--- main.js
+++ main.js
@@ -39,43 +39,41 @@ function play() {
   let playerScore = 0;
   let computerScore = 0;
 
-  for (let index = 0; index < 5; index++) {
-    console.log("Round", index + 1);
-    let playerSelection = prompt("Enter your choice: ");
-    let computerSelection = getComputerChoice();
+  // for (let index = 0; index < 5; index++) {
+  console.log("Round", index + 1);
+  let playerSelection = prompt("Enter your choice: ");
+  let computerSelection = getComputerChoice();
 
-    console.log("Player choice: ", playerSelection);
-    console.log("Computer choice: ", computerSelection);
+  console.log("Player choice: ", playerSelection);
+  console.log("Computer choice: ", computerSelection);
 
-    const results = playRound(playerSelection, computerSelection);
+  const results = playRound(playerSelection, computerSelection);
 
-    switch (results) {
-      case "d":
-        console.log("Draw...");
-        break;
-      case "p":
-        console.log(
-          `You win! ${capitalizeFirstLetter(
-            playerSelection
-          )} beats ${capitalizeFirstLetter(computerSelection)}!`
-        );
-        playerScore++;
-        break;
-      case "c":
-        console.log(
-          `You loose! ${capitalizeFirstLetter(
-            computerSelection
-          )} beats ${capitalizeFirstLetter(playerSelection)}!`
-        );
-        computerScore++;
-        break;
-    }
-    console.log(" ");
-
+  switch (results) {
+    case "d":
+      console.log("Draw...");
+      break;
+    case "p":
+      console.log(
+        `You win! ${capitalizeFirstLetter(
+          playerSelection
+        )} beats ${capitalizeFirstLetter(computerSelection)}!`
+      );
+      playerScore++;
+      break;
+    case "c":
+      console.log(
+        `You loose! ${capitalizeFirstLetter(
+          computerSelection
+        )} beats ${capitalizeFirstLetter(playerSelection)}!`
+      );
+      computerScore++;
+      break;
   }
+  console.log(" ");
+
+  // }
 
   console.log("Player Score: ", playerScore);
   console.log("Computer Score: ", computerScore);
 }
-
-play();
